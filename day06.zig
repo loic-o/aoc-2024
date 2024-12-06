78a251e5b21ad08893c7b6f7a8f9e4ed9d4a9414
loic <loicodle@gmail.com>
2024-12-06 16:11:13 -0500

    day 06, part 1 complete

---
+++ day06.zig
@@ -0,0 +1,193 @@
+const std = @import("std");
+// answers:
+// p1: 5086
+// p2: ????
+
+const puzzle_input = @embedFile("day06_data.txt");
+const test_input =
+    \\....#.....
+    \\.........#
+    \\..........
+    \\..#.......
+    \\.......#..
+    \\..........
+    \\.#..^.....
+    \\........#.
+    \\#.........
+    \\......#...
+;
+
+pub fn main() !void {
+    std.debug.print("Day 06 ----\n", .{});
+
+    const p1 = try part_one(puzzle_input);
+    std.debug.print("part one: {}\n", .{p1});
+
+    const p2 = try part_two(puzzle_input);
+    std.debug.print("part two: {}\n", .{p2});
+}
+
+const Direction = enum {
+    up,
+    down,
+    left,
+    right,
+};
+
+fn turnRight(direction: Direction) Direction {
+    return switch (direction) {
+        .up => .right,
+        .down => .left,
+        .left => .up,
+        .right => .down,
+    };
+}
+
+fn reverse(direction: Direction) Direction {
+    return switch (direction) {
+        .up => .down,
+        .down => .up,
+        .left => .right,
+        .right => .left,
+    };
+}
+
+const Board = struct {
+    allocator: std.mem.Allocator,
+    lines: [][]const u8,
+    width: usize,
+    height: usize,
+    row: usize,
+    col: usize,
+
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator, input: []const u8) !Self {
+        var line_list = std.ArrayList([]const u8).init(allocator);
+        defer line_list.deinit();
+
+        var line_reader = std.mem.tokenizeScalar(u8, input, '\n');
+        while (line_reader.next()) |line| {
+            try line_list.append(line);
+        }
+
+        const my_lines = try line_list.toOwnedSlice();
+        return Self{
+            .allocator = allocator,
+            .lines = my_lines,
+            .width = my_lines[0].len,
+            .height = my_lines.len,
+            .col = 0,
+            .row = 0,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.lines);
+    }
+
+    pub fn moveToChar(self: *Self, needle: u8) !void {
+        for (self.lines, 0..) |line, r| {
+            if (std.mem.indexOfScalar(u8, line, needle)) |c| {
+                self.row = r;
+                self.col = c;
+                return;
+            }
+        }
+        return error.CharNotFound;
+    }
+
+    pub fn move(self: *Self, direction: Direction) ?u8 {
+        switch (direction) {
+            .up => {
+                if (self.row > 0) {
+                    self.row -= 1;
+                    return self.lines[self.row][self.col];
+                }
+                return null;
+            },
+            .down => {
+                if (self.row < self.height - 1) {
+                    self.row += 1;
+                    return self.lines[self.row][self.col];
+                }
+                return null;
+            },
+            .left => {
+                if (self.col > 0) {
+                    self.col -= 1;
+                    return self.lines[self.row][self.col];
+                }
+                return null;
+            },
+            .right => {
+                if (self.col < self.width - 1) {
+                    self.col += 1;
+                    return self.lines[self.row][self.col];
+                }
+                return null;
+            },
+        }
+    }
+};
+
+fn part_one(input: []const u8) !usize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var board = try Board.init(allocator, input);
+    defer board.deinit();
+    try board.moveToChar('^');
+
+    std.debug.print("board: ({}x{}) st: ({}, {})\n", .{ board.width, board.height, board.col, board.row });
+
+    var bitset = try std.DynamicBitSet.initEmpty(allocator, board.width * board.height);
+    defer bitset.deinit();
+    bitset.set(board.row * board.width + board.col);
+
+    var dir = Direction.up;
+
+    while (board.move(dir)) |c| {
+        if (c == '#') {
+            _ = board.move(reverse(dir));
+            std.debug.print("(#<{},{}),", .{ board.col, board.row });
+            dir = turnRight(dir);
+        } else {
+            std.debug.print("({},{}),", .{ board.col, board.row });
+            bitset.set(board.row * board.width + board.col);
+        }
+    }
+    std.debug.print("\n", .{});
+
+    return bitset.count();
+}
+
+fn part_two(input: []const u8) !usize {
+    _ = input;
+    return 0;
+}
+
+test "part one" {
+    const expected: usize = 41;
+    const actual = try part_one(test_input);
+
+    std.testing.expect(expected == actual) catch |err| {
+        std.debug.print("part one FAIL.  expected: {}, got: {}\n", .{ expected, actual });
+        return err;
+    };
+
+    std.debug.print("part one PASSED.\n", .{});
+}
+
+test "part two" {
+    const expected: usize = 0;
+    const actual = try part_two(test_input);
+
+    std.testing.expect(expected == actual) catch |err| {
+        std.debug.print("part two FAIL.  expected: {}, got: {}\n", .{ expected, actual });
+        return err;
+    };
+
+    std.debug.print("part two PASSED.\n", .{});
+}
